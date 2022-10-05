dae16dda669d3107bbfac247a9bca23919325b56
khayratdinov <khayratdinov.np@gmail.com>
2022-10-05 12:19:05 +0500

    chore: 🎉 start Chapter 03 & copy Chapter 02 files

---
+++ Chapter-03/website/src/project/apps/categories/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from project.apps.core.model_fields import MultilingualCharField
+
+
+class Category(models.Model):
+    title = MultilingualCharField(
+        _("Title"),
+        max_length=200,
+    )
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.title
+
+    def get_ideas_without_this_category(self):
+        from project.apps.ideas.models import Idea
+        return Idea.objects.exclude(categories=self)

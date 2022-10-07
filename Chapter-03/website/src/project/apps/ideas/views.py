05020866c9e3bf49c41917618013236b0415769c
khayratdinov <khayratdinov.np@gmail.com>
2022-10-07 07:32:51 +0500

    chore: 🔨 Managing paginated lists

--- Chapter-03/website/src/project/apps/ideas/views.py
+++ Chapter-03/website/src/project/apps/ideas/views.py
@@ -5,7 +5,7 @@ from django.forms import modelformset_factory
 
 from .forms import IdeaForm, IdeaTranslationsForm
 from .models import Idea, IdeaTranslations
-
+from django.core.paginator import (EmptyPage, PageNotAnInteger, Paginator)
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
@@ -13,13 +13,14 @@ from django.conf import settings
 from .forms import IdeaFilterForm
 from .models import Idea, RATING_CHOICES
 
+
 PAGE_SIZE = getattr(settings, "PAGE_SIZE", 24)
 
 
 def idea_list(request):
+
     qs = Idea.objects.order_by("title")
     form = IdeaFilterForm(data=request.GET)
-
     facets = {
         "selected": {},
         "categories": {
@@ -37,24 +38,40 @@ def idea_list(request):
             ("rating", "rating"),
         )
         qs = filter_facets(facets, qs, form, filters)
-    
 
+    paginator = Paginator(qs, PAGE_SIZE)
+    page_number = request.GET.get("page")
+    try:
+        page = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer, show first page.
+        page = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, show last existing page.
+        page = paginator.page(paginator.num_pages)
 
-    context = {"form": form, "facets": facets, "object_list": qs}
+    context = {
+        "form": form,
+        "facets": facets,
+        "object_list": page,
+    }
     return render(request, "ideas/idea_list.html", context)
+
 
 def filter_facets(facets, qs, form, filters):
     for query_param, filter_param in filters:
-        value = form.cleaned_data[query_param] 
+        value = form.cleaned_data[query_param]
         if value:
             selected_value = value
             if query_param == "rating":
-                rating = int(value) 
-                selected_value = (rating, dict(RATING_CHOICES)[rating]) 
-            facets["selected"][query_param] = selected_value 
+                rating = int(value)
+                selected_value = (rating, dict(RATING_CHOICES)[rating])
+            facets["selected"][query_param] = selected_value
             filter_args = {filter_param: value}
             qs = qs.filter(**filter_args).distinct()
     return qs
+
+
 class IdeaList(ListView):
     model = Idea
 

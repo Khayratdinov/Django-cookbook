510d71afc034efef2bcd5a5219965c315c40bb98
khayratdinov <khayratdinov.np@gmail.com>
2022-10-14 15:33:03 +0500

    chore: 🔧 Open object details in a modal dialog

--- Chapter-04/website/src/project/apps/locations/urls.py
+++ Chapter-04/website/src/project/apps/locations/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from .views import LocationList, LocationDetail
 
+
 urlpatterns = [
     path("", LocationList.as_view(), name="location_list"),
+    path("add/", add_or_change_location, name="add_location"),
     path("<uuid:pk>/", LocationDetail.as_view(), name="location_detail"),
+    path(
+        "<uuid:pk>/modal/",
+        LocationDetail.as_view(
+            template_name="locations/location_detail_modal.html"),
+        name="location_detail_modal",
+    ),
 ]
--- Chapter-04/website/src/project/site_static/site/js/list.js
+++ Chapter-04/website/src/project/site_static/site/js/list.js
@@ -9,3 +9,27 @@ jQuery(function ($) {
     contentSelector: '.item,.pagination'
   }); 
 });
+
+
+jQuery(function ($) {
+  var $list = $('.item-list');
+  var $modal = $('#modal');
+  $modal.on('click', '.close', function (event) {
+    $modal.modal('hide');
+    // do something when dialog is closed... 
+  });
+  $list.on('click', 'a.item', function (event) { 
+    var $link = $(this);
+    var url = $link.data('modal-url');
+    var title = $link.data('modal-title');
+    if (url && title) {
+      event.preventDefault();
+      $('.modal-title', $modal).text(title); 
+      $('.modal-body', $modal).load(url, function () {
+        $modal.on('shown.bs.modal', function () { 
+          // do something when dialog is shown...
+        }).modal('show'); 
+      });
+    } 
+  });
+});

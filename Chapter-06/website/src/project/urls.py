fad2f5a9716506b38b0437416db31b4c4bed109d
Nurpolat <khayratdinov.np@gmail.com>
2022-11-02 15:00:16 +0500

    chore: 🔨 Get user avatar (Gravatar)

--- Chapter-06/website/src/project/urls.py
+++ Chapter-06/website/src/project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
@@ -8,9 +8,9 @@ from django.shortcuts import redirect
 from project.apps.core import views as core_views
 
 urlpatterns = i18n_patterns(
+    path("admin/", admin.site.urls),
     path("", lambda request: redirect("locations:location_list")),
     # path("", lambda request: redirect("ideas:idea_list")),
-    path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("js-settings/", core_views.js_settings, name="js_settings"),
     path(
@@ -22,17 +22,5 @@ urlpatterns = i18n_patterns(
 )
 
 
-urlpatterns += [
-    path(
-        "upload-file/",
-        core_views.upload_file,
-        name="upload_file",
-    ),
-    path(
-        "delete-file/",
-        core_views.delete_file,
-        name="delete_file",
-    ),
-]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static("/media/", document_root=settings.MEDIA_ROOT)

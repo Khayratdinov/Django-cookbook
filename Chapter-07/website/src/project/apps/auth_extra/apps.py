7b64917bf5ec1180d3d653a4f5152ef25b11d634
Nurpolat <khayratdinov.np@gmail.com>
2022-11-06 15:05:32 +0500

    chore: 🎨 updated Chapter 07 files

--- Chapter-07/website/src/project/apps/auth_extra/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthExtraConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "project.apps.auth_extra"
--- Chapter-07/website/src/project/urls.py
+++ Chapter-07/website/src/project/urls.py
@@ -1,19 +1,16 @@
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
 
-from project.apps.core import views as core_views
-
 urlpatterns = i18n_patterns(
-    # path("admin/", include("admin_honeypot.urls", namespace="admin_honeypot")),
+    path("", lambda request: redirect("ideas:idea_list")),
     path("admin/", admin.site.urls),
-    path("js-settings/", core_views.js_settings, name="js_settings"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("ideas/", include(("project.apps.ideas.urls", "ideas"), namespace="ideas")),
     # path("search/", include("haystack.urls")),
 )
-
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static("/media/", document_root=settings.MEDIA_ROOT)
